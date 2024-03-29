5e979811e18fe4110831339b441f527fa5fff009
SextyNine <mohammad87115@gmail.com>
2024-03-29 08:45:06 +0330

    sex

---
+++ index.js
@@ -0,0 +1,62 @@
+// Importing discord.js, chalk & logger
+const { Client, GatewayIntentBits, Collection } = require("discord.js");
+const logger = require("./functions/logging.js");
+const chalk = require("chalk")
+
+// Creating a new Client
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.DirectMessages
+    ]
+});
+
+/* ----- Variables ----- */
+client.commands = new Collection();
+client.messageCommands = new Collection();
+client.config = require("./config.js")
+
+/* ----- Anti Crash -----*/
+
+process.on("unhandledRejection", (reason, p) => {
+    console.log(chalk.gray("————————————————————————————————————————————————————"));
+    console.log(
+       chalk.white("["),
+       chalk.red.bold("AntiCrash"),
+       chalk.white("]"),
+       chalk.gray(" : "),
+       chalk.white.bold("Unhandled Rejection/Catch")
+    );
+    console.log(chalk.gray("————————————————————————————————————————————————————"));
+    console.log(reason, p);
+ });
+ process.on("uncaughtException", (err, origin) => {
+    console.log(chalk.gray("————————————————————————————————————————————————————"));
+    console.log(
+       chalk.white("["),
+       chalk.red.bold("AntiCrash"),
+       chalk.white("]"),
+       chalk.gray(" : "),
+       chalk.white.bold("Uncaught Exception/Catch")
+    );
+    console.log(chalk.gray("————————————————————————————————————————————————————"));
+    console.log(err, origin);
+ });
+ process.on("multipleResolves", (type, promise, reason) => {
+    console.log(chalk.gray("————————————————————————————————————————————————————"));
+    console.log(
+       chalk.white("["),
+       chalk.red.bold("AntiCrash"),
+       chalk.white("]"),
+       chalk.gray(" : "),
+       chalk.white.bold("Multiple Resolves")
+    );
+    console.log(chalk.gray("————————————————————————————————————————————————————"));
+    console.log(type, promise, reason);
+ });
+
+
+/* ----- Logging in ----- */
+logger.log("Trying to login with the token ...")
+client.login(client.config.token)
