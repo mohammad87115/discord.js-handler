ffda75e8b9b094cb139b63ce51b04b093a178ab6
mohammad87115 <mohammad87115@gmail.com>
2024-06-06 12:58:27 +0330

    sex

--- config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    token: "MTEwMzk2MjM1MDk3MDc1MzAzNQ.GkYPrW.7ndnz1PWYrqZKIS_LrN9--6vm-x5dcYPDczKgw",
-    prefix: "-",
-    color: "#32a897",
-    ownerIds: ["988433294498607115"],
-}
